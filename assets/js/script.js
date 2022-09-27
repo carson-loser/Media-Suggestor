463f8e6faa30535ff9a7697700b03793f074ec06
Andrew <andrewrandolph98@gmail.com>
2022-09-27 00:49:53 -0500

    API's added and working as well as formatting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,40 @@
-var spotifyAPI = "https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10";
-var youtubeAPI = "https://www.googleapis.com/youtube/v3";
-var dropdownMenu = document.querySelector('.dropdown-trigger'); 
+// var spotifyAPI = "https://api.spotify.com/v1/artists/1vCWHaC5f2uS3yhpwWbIA6/albums?album_type=SINGLE&offset=20&limit=10";
+// var youtubeAPI = "https://www.googleapis.com/youtube/v3";
+// var dropdownMenu = document.querySelector('.dropdown-trigger'); 
 
 
-$(document).ready(function(){
-  $(dropdownMenu).formSelect();
+// $(document).ready(function(){
+//   $(dropdownMenu).formSelect();
+// });
+
+
+const settingsSPOT = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=10&numberOfTopResults=5",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "e88899cff1mshe2370cf428284dap1855a0jsn65cf40418126",
+		"X-RapidAPI-Host": "spotify23.p.rapidapi.com"
+	}
+};
+
+$.ajax(settingsSPOT).done(function (response) {
+	console.log(response);
+});
+
+
+const settingsYT = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://youtube138.p.rapidapi.com/auto-complete/?q=musicvideos&hl=en&gl=US",
+	"method": "GET",
+	"headers": {
+		"X-RapidAPI-Key": "e88899cff1mshe2370cf428284dap1855a0jsn65cf40418126",
+		"X-RapidAPI-Host": "youtube138.p.rapidapi.com"
+	}
+};
+
+$.ajax(settingsYT).done(function (response) {
+	console.log(response);
 });
